bd1bf33aae279dc0e91b13820b80e2369815fb29
adrirocha <112907054+adrirocha@users.noreply.github.com>
2025-03-22 16:43:55 -0300

    Create game_data.py

---
+++ Higher Lower Game/game_data.py
@@ -0,0 +1,50 @@
+profiles = [
+    {
+        'name': 'Cristiano Ronaldo',
+        'level': 100,
+        'description': 'Professional Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Lionel Messi',
+        'level': 99,
+        'description': 'Professional Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'level': 98,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ariana Grande',
+        'level': 97,
+        'description': 'Singer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'level': 96,
+        'description': 'Singer',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Neymar Jr.',
+        'level': 95,
+        'description': 'Professional Footballer',
+        'country': 'Brazil'
+    },
+    {
+        'name': 'Real Madrid',
+        'level': 94,
+        'description': 'Professional Football Club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'NASA',
+        'level': 93,
+        'description': 'Government Agency',
+        'country': 'United States'
+    }
+]
